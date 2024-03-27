7257d83bbd63ec2159d27790c9add71491588134
quectel_lte <Product_ruiwei@163.com>
2024-03-27 16:17:11 +0800

    调整项目结构
    调整项目结构

--- docs/.vuepress/sidebar.ts
+++ docs/.vuepress/sidebar.ts
@@ -2,17 +2,26 @@ import { sidebar } from "vuepress-theme-hope";
 
 // 精选图标：https://theme-hope.vuejs.press/zh/guide/interface/icon.html#iconfont-%E7%B2%BE%E9%80%89%E5%9B%BE%E6%A0%87
 export default sidebar({
-  "": [
+    "": [
 
-    // 指定显示页面
-    {
-        text: "🧑‍💻协议积累",
-        icon: "",
-        prefix: "/protocol/",
-        collapsible: true,
-        children: ["zigbee-table.md", "zigbee-routing.md"],
-      },
-  ],
-  // 专题区
-  "/apps/topic/": "structure",
+        // 指定显示页面
+        {
+            text: "🧑‍💻zigbee",
+            icon: "zigbee",
+            prefix: "/protocol/ZigBee",
+            collapsible: true,
+            children: ["zigbee-table.md", "zigbee-routing.md"],
+        },
+
+        // 指定显示页面
+        {
+            text: "LTE",
+            icon: "lte",
+            prefix: "/protocol/LTE",
+            collapsible: false,
+            children: ["sim.md"],
+        },
+    ],
+    // 专题区
+    "/apps/topic/": "structure",
 });
